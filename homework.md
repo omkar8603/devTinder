fec95566f460e820d9c9c8730de2fdc0600af88e
Omkar Mane <omkarmane8603@gmail.com>
2025-05-02 22:19:22 +0530

    get APIs

--- homework.md
+++ homework.md
@@ -40,3 +40,6 @@
  - JS Object vs JSON  (difference)
  - Add the express.json middleware to your app
  - Make your signup API dynamic to receive data from the end  user
+ - API - Get user by email'
+ - API - Feed API - GET /feed - get all the users from the database
+   
--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ app.use(express.json());
 
 
 app.post("/signup", async (req, res) => {
-
+   
+    // creating the instance of the model
     const user = new User(req.body);
     try {
        
@@ -21,6 +22,40 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+ 
+app.get('/user', async (req, res) => {
+    const emailId = req.body.email;
+    
+
+    try { 
+        const user = await User.find({email : emailId})
+        
+        if (user?.length === 0){
+            console.log("user Not found")
+            res.status(404).send("user not found");
+        } else{
+            res.status(200).send({user : user});
+        }
+      
+    } catch (error) {
+        console.error(error);
+        res.status(400).send("Something Went wrong")
+    }
+})
+
+
+// Feed API - GET /feed - get all the users from the database
+app.get('/feed', async (req, res) => {
+    try {
+        
+        const data = await User.find()
+
+        res.status(200).send({data : data});
+    } catch (error) {
+        console.error(error);
+        res.status(400).send("Error Saving the User")
+    }
+})
 
 
 
